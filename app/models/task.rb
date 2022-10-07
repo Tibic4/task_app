e07b34ff16ad02fb366ee1ff2daa8a59a6410ea8
Alton Vieira <bicagold@gmail.com>
2022-10-07 14:07:26 -0300

    feat: validade name not included comma

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,9 @@
 class Task < ApplicationRecord
   validates :name, presence: true
+
+  validate :validate_name_not_including_comma
+
+  def validate_name_not_including_comma
+    errors.add(:name, 'cannot include comma') if name&.include?(',')
+  end
 end
