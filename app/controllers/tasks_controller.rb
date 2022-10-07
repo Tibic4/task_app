c229095bff4d79caf8f4ea588837270a524a7504
Alton Vieira <bicagold@gmail.com>
2022-10-07 13:32:02 -0300

    feat: task controller create action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,6 +9,18 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
+  def create
+    task = Task.new(task_params)
+    task.save!
+    redirect_to tasks_path, notice: "Task '#{task.name}' was successfully created."
+  end
+
   def edit
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description)
   end
 end
